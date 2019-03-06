eb6277a5dbb6baa1c2e8d18b7d2b10894543be43
alinaut <bykovskaya.alina@gmail.com>
2019-03-06 23:11:38 +0300

    ring list is done

--- src/theadring.cpp
+++ src/theadring.cpp
@@ -1,2 +1,30 @@
 #include "theadring.h"
 
+THeadRing::THeadRing() :TDatList()
+{
+	InsLast();
+	pHead = pFirst;
+	ListLen = 0;
+	pStop = pHead;
+	Reset();
+	pFirst->SetNextLink(pFirst);
+}
+
+THeadRing::~THeadRing()
+{
+	TDatList::DelList();
+	DelLink(pHead);
+	pHead = nullptr;
+}
+
+void THeadRing::InsFirst(PTDatValue pVal)
+{
+	TDatList::InsFirst(pVal);
+	pHead->SetNextLink(pFirst);
+}
+
+void THeadRing::DelFirst()
+{
+	TDatList::DelFirst();
+	pHead->SetNextLink(pFirst);
+}
