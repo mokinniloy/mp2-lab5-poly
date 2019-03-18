3426eafadc1b909e9ab5bef4b069fe9c63df7730
DimaSaz <st132814@gmail.com>
2019-03-18 23:50:24 +0300

    fif_proc

---
+++ include/DatLink.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "DatValue.h"
+#include "RootLink.h"
+
+
+class TDatLink;
+typedef TDatLink *PTDatLink;
+
+class TDatLink : public TRootLink
+{
+protected:
+	PTDatValue pValue; // указатель на объект значения
+public:
+	TDatLink(PTDatValue pVal = NULL, PTRootLink pN = NULL) : TRootLink(pN)
+	{
+		pValue = pVal;
+	}
+	void SetDatValue(PTDatValue pVal) { pValue = pVal; }
+	PTDatValue GetDatValue() { return pValue; }
+	PTDatLink GetNextDatLink() { return (PTDatLink)pNext; }
+
+	friend class TDatList;
+};
+
